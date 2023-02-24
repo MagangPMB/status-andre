d0caed05e8d884a499a5aa10409791019a0ecd07
andreee6 <sembiringkembaren046@gmail.com>
2023-02-24 10:40:43 +0700

    commit andre

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $databases = "db_status";
 
-// Create connection
+// Create connection to database
 $conn = mysqli_connect($servername, $username, $password, $databases);
 
 // Check connection
